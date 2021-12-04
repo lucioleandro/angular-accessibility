726f3a37aced9c9845f6b227d7f0f1ffa7019de3
Lúcio Leandro <lucio.leo85@gmail.com>
2021-12-04 17:27:31 -0300

    yes-no-button css + implementation

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-yes-no-button-group',
@@ -9,10 +9,25 @@ export class YesNoButtonGroupComponent implements OnInit {
 
   @Input() value: string = null;
   @Input() label: string = '';
-  
+
+  @Output() valueChange = new EventEmitter<string>();
+
+  options = YesNoButtonGroupOptions;
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  activate(value: string): void  {
+    this.value = value
+    this.valueChange.emit(this.value);
+  }
+
+}
+
+enum YesNoButtonGroupOptions {
+  YES = 'yes',
+  NO = 'no'
 }
